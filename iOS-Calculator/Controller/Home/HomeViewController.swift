f5ec464acca90fd8ea05cd3c3e3850558f861386
RaulPascual <raulpascual63@gmail.com>
2021-09-10 09:15:24 +0200

    Project set up

---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Raúl Pascual de la Calle on 10/9/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    //MARK: - Init
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Lice Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
