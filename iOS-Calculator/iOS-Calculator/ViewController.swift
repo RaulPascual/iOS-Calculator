71d2dadcc19227771b2f412eb9de3fbfb328c1bb
RaulPascual <raulpascual63@gmail.com>
2021-09-10 08:26:52 +0200

    Creating the project

---
+++ iOS-Calculator/iOS-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Calculator
+//
+//  Created by Raúl Pascual de la Calle on 10/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
